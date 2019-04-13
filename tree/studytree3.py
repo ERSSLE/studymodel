c771375f3b4d9b28fee626ec297f2bfc6f588c36
ERSSLE <253188236@qq.com>
2019-04-13 16:38:35 +0800

    ...

--- tree/studytree3.py
+++ tree/studytree3.py
@@ -10,11 +10,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeRegressor
 
-#
+#创建随机数据集
 rng = np.random.RandomState(1)
 X = np.sort(200 * rng.rand(100,1) - 100,axis=0)
 y = np.array([np.pi * np.sin(X).ravel(),np.pi * np.cos(X).ravel()]).T
-y[::5,:] += (0.5 - rnd.rand(20,2))
+y[::5,:] += (0.5 - rng.rand(20,2))
 
-#
+#创建回归模型
 regr_1 = DecisionTreeRegressor(max_depth=2)
+regr_2 = DecisionTreeRegressor(max_depth=5)
+regr_3 = DecisionTreeRegressor(max_depth=8)
+regr_1.fit(X,y)
+regr_2.fit(X,y)
+regr_3.fit(X,y)
+
+#不同深度决策树预测
+X_test = np.arange(-100.0,100.0,0.01)[:,np.newaxis]
+y_1 = regr_1.predict(X_test)
+y_2 = regr_2.predict(X_test)
+y_3 = regr_3.predict(X_test)
+
+#预测结果可视化
+plt.figure()
+s=25
+plt.scatter(y[:,0],y[:,1],c='navy',s=s,edgecolor='black',
+            label='data')
+plt.scatter(y_1[:,0],y_1[:,1],c='cornflowerblue',s=s,
+            edgecolor='black',label='max_depth=2')
+plt.scatter(y_2[:,0],y_2[:,1],c='red',s=s,
+            edgecolor='black',label='max_depth=5')
+plt.scatter(y_3[:,0],y_3[:,1],c='orange',s=s,
+            edgecolor='black',label='max_depth=8')
+plt.xlim([-6,6])
+plt.ylim([-6,6])
+plt.xlabel('target1')
+plt.ylabel('target2')
+plt.title('Muti-output Decison Tree Regression')
+plt.legend(loc='best')
+plt.show()
+
+
+
+
+
+
